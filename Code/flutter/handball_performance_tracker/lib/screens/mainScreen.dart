c3dbdb4568e5009f70a0dac9569034ce394790c6
Daniel <Daniel.wag@gmx.net>
2022-05-13 12:20:37 +0200

    add player names and selection

--- Code/flutter/handball_performance_tracker/lib/screens/mainScreen.dart
+++ Code/flutter/handball_performance_tracker/lib/screens/mainScreen.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 // import 'package:get/get.dart';
 
-class mainScreen extends StatelessWidget {
-  // final MainScreenController controller = Get.put(MainScreenController());
+class mainScreen extends StatefulWidget {
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
 
+class _mainScreenState extends State<mainScreen> {
+  List<String> playerNames = [];
+  List<TextEditingController> playerNameControllers = [];
+  String selectedPlayer = "";
 
   @override
   Widget build(BuildContext context) {
+    for (int i = 1; i < 7; i++) {
+      playerNameControllers.add(TextEditingController());
+    }
     return Scaffold(
-      appBar: AppBar(
-       title: Text("Title")
-      ),
+      appBar: AppBar(title: Text("Title")),
       body: ListView.builder(
           padding: const EdgeInsets.all(8),
           itemCount: 7,
           itemBuilder: (BuildContext context, int index) {
             return TextField(
-              // controller: controller.playerNameTextControllers[index],
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
+              onTap: () {
+                selectedPlayer = playerNameControllers[index].value.text;
+                print("sel player: " + selectedPlayer);
+              },
+              controller: playerNameControllers[index],
             );
-          }
-      ),
+          }),
     );
   }
 }
