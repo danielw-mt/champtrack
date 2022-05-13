effe2ac1b4d27686bd7d9e5e7087d02ed5e11869
Daniel <Daniel.wag@gmx.net>
2022-05-13 18:05:13 +0200

    fix firebase issues

--- Code/flutter/handball_performance_tracker/lib/main.dart
+++ Code/flutter/handball_performance_tracker/lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'screens/mainScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'dart:io';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MaterialApp(
-      title: 'Handball Performance Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: mainScreen()));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,).then((value)async  {
+    // check internet connectivity
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        print('connected');
+        // pull from firebase to synchronize
+        FirebaseFirestore db = FirebaseFirestore.instance;
+      }
+    } on SocketException catch (_) {
+      print('not connected');
+    }
+    // start app
+    runApp(MaterialApp(
+        title: 'Handball Performance Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: mainScreen()));
+  }).timeout(const Duration (seconds:5),onTimeout : onTimeout());
+
 }
 
-void startFirebase() async {}
+onTimeout() {
+  runApp(MaterialApp(title: "Timeout"));
+}
+
