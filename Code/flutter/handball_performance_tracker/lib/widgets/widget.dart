171a34079e43d824c1a9a650d971df7e9b3adaf0
Janina Krueger <janina.krueger@fau.de>
2022-05-20 15:44:17 +0200

    Add possibility to swipe between left and right field side.
    Implemented Widget FieldSwitch which uses the flutter class PageView to make
    it possible to swipe.
    
    Ref Issue #65

--- Code/flutter/handball_performance_tracker/lib/widgets/widget.dart
+++ Code/flutter/handball_performance_tracker/lib/widgets/widget.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:handball_performance_tracker/utils/helper.dart';
-import 'package:handball_performance_tracker/controllers/fieldSizeParameter.dart'
-    as fieldSizeParameter;
 
 class CustomField extends StatelessWidget {
-  CustomField({Key? key}) : super(key: key);
+  const CustomField({Key? key, required this.leftSide}) : super(key: key);
 
   // Parameter to determine if right or left field side is shown
-  bool leftSide = true;
+  final bool leftSide;
 
   @override
   Widget build(BuildContext context) {
     //SizedBox around! und darum FittedBox!
-    return SizedBox(
-      width: fieldSizeParameter.fieldWidth,
-      height: fieldSizeParameter.fieldHeight,
-      child: CustomPaint(
-        painter: FieldPainter(leftSide),
-        // GestureDetector to get coordinates on click
-        child: GestureDetector(
-          onTapDown: (TapDownDetails details) =>
-              SectorCalc(leftSide).calculatePosition(details.localPosition),
-        ),
+    return CustomPaint(
+      painter: FieldPainter(leftSide),
+      // GestureDetector to handle on click or swipe
+      child: GestureDetector(
+        // handle coordinates on click
+        onTapDown: (TapDownDetails details) =>
+            SectorCalc(leftSide).calculatePosition(details.localPosition),
       ),
+    );
+  }
+}
+
+/*
+* Uses Pageview to make it possible to swipe between left and right field side.
+* @return   Returns a Pageview with left field side and right field side as children.
+*/
+class FieldSwitch extends StatelessWidget {
+  const FieldSwitch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: <Widget>[
+        const CustomField(
+          leftSide: true,
+        ),
+        const CustomField(
+          leftSide: false,
+        ),
+      ],
     );
   }
 }
