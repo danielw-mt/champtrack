4b95a98844fbb4fe966cb97cdbbe2eedbd7d05ab
Annika Mücke <annika-muecke@outlook.de>
2022-05-29 15:37:40 +0200

    add constants required for ef score calculation

--- Code/flutter/handball_performance_tracker/lib/constants/positions.dart
+++ Code/flutter/handball_performance_tracker/lib/constants/positions.dart
@@ -1,5 +1,5 @@
-const int inSix = 0; 
-const int betweenSixAndNine = 1;
-const int outsideNine = 2; 
-const List<String> throwPositions = ["<6", "6to9", ">9"];
+const String inSix = "<6"; 
+const String betweenSixAndNine = "6to9";
+const String outsideNine = ">9"; 
+const List<String> throwPositions = [inSix, betweenSixAndNine, outsideNine];
 const List<String> sectors = ["1", "2", "3", "4", "5", "6"]; // TODO: replace by meaningful player position names
