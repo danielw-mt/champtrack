11a4e267a1dc5e0da41c2ef6803d70f241a3e56e
Vincent Gemar <vincent.gemvin.gemar@fau.de>
2022-06-01 17:19:03 +0200

    Modularized strings in nav_drawer file

--- Code/flutter/handball_performance_tracker/lib/main.dart
+++ Code/flutter/handball_performance_tracker/lib/main.dart
@@ -7,6 +7,7 @@ import 'config/firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'Strings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
