7a4d32409401c7902b42f42ca17aa2609e4a54f6
Daniel <Daniel.wag@gmx.net>
2022-06-05 14:59:02 +0200

    deactivate on_field_checkbox for now

--- Code/flutter/handball_performance_tracker/lib/widgets/settings_screen/on_field_checkbox.dart
+++ Code/flutter/handball_performance_tracker/lib/widgets/settings_screen/on_field_checkbox.dart
@@ -13,21 +13,22 @@ class OnFieldCheckbox extends StatelessWidget {
   Widget build(BuildContext context) {
     var playersOnField = globalController.playersOnField;
 
+    // TODO adapt this to new player variables
     return GetBuilder<GlobalController>(
         builder: (_) => Checkbox(
-              value: playersOnField[index],
+              value: true, //playersOnField[index],
               onChanged: (value) {
                 // count how many players are selected as starting players
                 var numOnField =
                     playersOnField.where((c) => c == true).toList().length;
                 // when you try to check a checkbox it should only be possible with less than 7 players
-                if (value == true) {
-                  if (numOnField < 7) {
-                    playersOnField[index] = true;
-                  }
-                } else {
-                  playersOnField[index] = false;
-                }
+                // if (value == true) {
+                //   if (numOnField < 7) {
+                //     playersOnField[index] = true;
+                //   }
+                // } else {
+                //   playersOnField[index] = false;
+                // }
                 globalController.refresh();
               },
             ));
