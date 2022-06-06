e46effd0193f1d20c652c9530d74c19d8a4f00df
Daniel <Daniel.wag@gmx.net>
2022-06-06 14:06:37 +0200

    move global controller put back

--- Code/flutter/handball_performance_tracker/lib/screens/mainScreen.dart
+++ Code/flutter/handball_performance_tracker/lib/screens/mainScreen.dart
@@ -10,7 +10,7 @@ import 'package:flutter/services.dart';
 
 class MainScreen extends StatelessWidget {
   // screen where the game takes place
-  GlobalController globalController = Get.find<GlobalController>();
+  final GlobalController globalController = Get.put(GlobalController());
 
   @override
   Widget build(BuildContext context) {
