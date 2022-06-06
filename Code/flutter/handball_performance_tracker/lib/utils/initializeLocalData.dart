608a48907ef8c1954167d5755691b7f8b5ee17d7
Daniel <Daniel.wag@gmx.net>
2022-06-06 15:06:05 +0200

    Create initializeLocalData.dart file

---
+++ Code/flutter/handball_performance_tracker/lib/utils/initializeLocalData.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../data/player.dart';
+import '../data/team.dart';
+import '../data/game.dart';
+import '../data/database_repository.dart';
+import 'package:get/get.dart';
+import '../controllers/globalController.dart';
+
+void initializeLocalData() {
+  DatabaseRepository repo = DatabaseRepository();
+  // init game
+
+  // teams
+  // every player
+}
