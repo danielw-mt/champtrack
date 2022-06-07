756cfc642d51ff126234c972c8e11a6f0ed1daff
Vincent Gemar <vincent.gemvin.gemar@fau.de>
2022-06-07 13:38:27 +0200

    added strings file for main, nav_drawer, game_actions screens

--- Code/flutter/handball_performance_tracker/lib/strings.dart
+++ Code/flutter/handball_performance_tracker/lib/strings.dart
@@ -1,15 +1,37 @@
+
 class Strings {
+// all language strings start with a small l in front of the variable
+
   // main.dart
   static const String app_title = "Title Town";
+  static const String lError = "Error";
+  static const String lConnectionCheck = "Checking connection...";
+  static const String lConnected = "connected";
+  static const String lNot = "not";
 
 
   // nav_drawer.dart
-  static const String sideMenuHeader = "Side menu";
-  static const String settings = "Settings";
-  static const String mainScreen = "Main Screen";
-  static const String helperScreen = "Helper Screen";
+  static const String lSideMenuHeader = "Side menu";
+  static const String lSettings = "Settings";
+  static const String lMainScreen = "Main Screen";
+  static const String lHelperScreen = "Helper Screen";
   
 
+  // game_actions.dart
+  static const String lRedCard = "Rote Karte";
+  static const String lYellowCard = "Gelbe Karte";
+  static const String lTimePenalty = "Zeitstrafe";
+  static const String lGoal = "Tor";
+  static const String lOneVsOne = "1v1 & 7m";
+  static const String lTwoMin = "2min ziehen";
+  static const String lErrThrow = "err";
+  static const String lTrf  = "TRF";
+  static const String lFoul7m = "Foul => 7m";
+  static const String lBlockNoBall = "Block ohne Ballgewinn";
+  static const String lBlockAndSteal = "Block & Steal";
 
+  //settingsScreen.dart
+  static const String lFieldSideIsRight = "Home goal is right side of screen";
+  
 
 }
