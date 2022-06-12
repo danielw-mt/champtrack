320e131f52ee77998d7b1c01d4bcf7c8dbdfb4bd
Daniel Wagner <dan.wagner@fau.de>
2022-06-12 12:09:41 +0200

    Implement basic sign in

---
+++ Code/flutter/handball_performance_tracker/lib/screens/authenticationScreen.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class AuthenticationScreen extends StatefulWidget {
+  const AuthenticationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthenticationScreen> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            height: 40,
+          ),
+          TextField(
+            controller: emailController,
+            textInputAction: TextInputAction.next,
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          TextField(
+            controller: passwordController,
+            textInputAction: TextInputAction.done,
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(onPressed: signIn, child: Text("Sign in")),
+        ]),
+      ),
+    );
+  }
+
+  Future signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim());
+  }
+}
