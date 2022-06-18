3aede5128dec88cc7a533039ae139346946c7cc7
Daniel Wagner <dan.wagner@fau.de>
2022-06-18 19:42:47 +0200

    Add comments

--- Code/flutter/handball_performance_tracker/lib/screens/startGameScreen.dart
+++ Code/flutter/handball_performance_tracker/lib/screens/startGameScreen.dart
@@ -13,12 +13,8 @@ import '../widgets/settings_screen/game_start_stop_buttons.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import '../screens/mainScreen.dart';
 
-// TODO change this to Statefulwidget
-// turn team selection screen into team selection screen and teamSelectionWidget
-// build player position widget as a simple list
-// build halbzeit seite auswählen widget
-// dependign on the State of StartGameScreen build StartGameForm,teamSelectionDropdown, PlayersList, PlayerPositionWidget, halbzeit seite auswählen widget, check everything widget
-
+/// Screen that contains a guided walkthrough for adjusting settings and
+/// selecting players before starting the game
 class StartGameScreen extends StatefulWidget {
   StartGameScreen({Key? key}) : super(key: key);
 
@@ -32,6 +28,7 @@ class _StartGameScreenState extends State<StartGameScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // after a hot reload the app crashes. This prevents it
     if (!Get.isRegistered<GlobalController>())
       return Column(
         children: [
@@ -44,7 +41,6 @@ class _StartGameScreenState extends State<StartGameScreen> {
               child: Text("Home"))
         ],
       );
-    print("Page $startGameFlowStep");
     return Scaffold(
         key: _scaffoldKey,
         drawer: NavDrawer(),
@@ -54,71 +50,12 @@ class _StartGameScreenState extends State<StartGameScreen> {
             // Container for menu button on top left corner
             MenuButton(_scaffoldKey),
             buildStartScreenContent(),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              ElevatedButton(
-                onPressed: () {
-                  if (startGameFlowStep > 0) {
-                    setState(() {
-                      startGameFlowStep -= 1;
-                    });
-                  } else {
-                    Get.to(Dashboard());
-                  }
-                },
-                child: startGameFlowStep == 0
-                    ? Text(Strings.lDashboard)
-                    : Text(Strings.lBack),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    // for first page and page 3 just go to next page normally
-                    if (startGameFlowStep == 0) {
-                      setState(() {
-                        print("page 0 or 2");
-                        startGameFlowStep = 1;
-                      });
-                      return;
-                    }
-                    if (startGameFlowStep == 1) {
-                      GlobalController globalController =
-                          Get.find<GlobalController>();
-                      if (globalController
-                              .selectedTeam.value.onFieldPlayers.length !=
-                          7) {
-                        Alert(
-                                context: context,
-                                title: Strings.lWarningPlayerNumberErrorMessage,
-                                type: AlertType.error,
-                                desc: Strings.lPlayerNumberErrorMessage)
-                            .show();
-                      } else {
-                        print("page 1 and correct");
-                        setState(() {
-                          startGameFlowStep = 2;
-                        });
-                        return;
-                      }
-                    }
-                    if (startGameFlowStep == 2) {
-                      setState(() {
-                        startGameFlowStep = 3;
-                      });
-                      return;
-                    }
-                    if (startGameFlowStep == 3) {
-                      startGame(context);
-                      Get.to(MainScreen());
-                      return;
-                    }
-                  },
-                  child: startGameFlowStep == 3
-                      ? Text(Strings.lStartGameButton)
-                      : Text(Strings.lNext))
-            ])
+            // buttons
           ],
         ));
   }
 
+  /// Depending on what page of the screen you are render different main content
   Widget buildStartScreenContent() {
     if (startGameFlowStep == 0) {
       return StartGameForm();
@@ -135,5 +72,76 @@ class _StartGameScreenState extends State<StartGameScreen> {
       return Text("Verifiy inputs");
     }
     return Container();
+  }
+
+  /// Buttons that allow to go to the next and last page in the flow
+  /// pages are set from 0 to 3 in startGameFlowStep
+  Widget buildBackNextButtons() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      // 'Back' button
+      ElevatedButton(
+        onPressed: () {
+          if (startGameFlowStep > 0) {
+            setState(() {
+              startGameFlowStep -= 1;
+            });
+          } else {
+            Get.to(Dashboard());
+          }
+        },
+        // when page 0 is reached change the text of the button
+        child: startGameFlowStep == 0
+            ? Text(Strings.lDashboard)
+            : Text(Strings.lBack),
+      ),
+      // 'Next' button
+      ElevatedButton(
+          onPressed: () {
+            // go to page 1
+            if (startGameFlowStep == 0) {
+              setState(() {
+                print("page 0 or 2");
+                startGameFlowStep = 1;
+              });
+              return;
+            }
+            // validate whether 7 players were selected on this page
+            if (startGameFlowStep == 1) {
+              GlobalController globalController = Get.find<GlobalController>();
+              // display alert if less than 7 have been selected
+              if (globalController.selectedTeam.value.onFieldPlayers.length !=
+                  7) {
+                Alert(
+                        context: context,
+                        title: Strings.lWarningPlayerNumberErrorMessage,
+                        type: AlertType.error,
+                        desc: Strings.lPlayerNumberErrorMessage)
+                    .show();
+              } else {
+                setState(() {
+                  startGameFlowStep = 2;
+                });
+                return;
+              }
+            }
+            // go to page 3
+            if (startGameFlowStep == 2) {
+              setState(() {
+                startGameFlowStep = 3;
+              });
+              return;
+            }
+            // start game from last page and go to mainscreen
+            if (startGameFlowStep == 3) {
+              startGame(context);
+              Get.to(MainScreen());
+              return;
+            }
+          },
+          // when page 3 is reached change the text of the button
+          child: startGameFlowStep == 3
+              ? Text(Strings.lStartGameButton)
+              : Text(Strings.lNext))
+    ]);
   }
 }
