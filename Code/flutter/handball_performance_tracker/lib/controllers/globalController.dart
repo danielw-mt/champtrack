82d9c816d379d9dc6ad88ebc1574c9b3b5b1a938
Daniel Wagner <dan.wagner@fau.de>
2022-06-19 15:54:05 +0200

    remove function

--- Code/flutter/handball_performance_tracker/lib/controllers/globalController.dart
+++ Code/flutter/handball_performance_tracker/lib/controllers/globalController.dart
@@ -93,12 +93,6 @@ class GlobalController extends GetxController {
   /// text to be displayed in the player menu title on the right side, changes after a goal
   RxString playerMenuText = "".obs;
 
-  // TODO this method is unnecessary unless we want getters and setters to exist for every method
-  void updatePlayerMenuText() {
-    // changing from dep = input.obs
-    playerMenuText.value = "Assist";
-  }
-
   /// corresponding player object for last clicked player name in the player menu
   Rx<Player> lastClickedPlayer = Player().obs;
 
