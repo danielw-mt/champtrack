2ee49cbc1fd6d59993353cbeb9a32d7b5a345c8e
Vincent Gemar <vincent.gemvin.gemar@fau.de>
2022-06-25 09:57:04 +0200

    added single string files instead of one big one

---
+++ Code/flutter/handball_performance_tracker/lib/constants/stringsGameScreen.dart
@@ -0,0 +1,34 @@
+class StringsGameScreen{
+
+  // stopwatch.dart
+  static const String lStartTime = "Start";
+  static const String lStopTime = "Stop";
+  static const String lResetTime = "Zurücksetzen";
+  static const String lPlusOneTime = "+1 Sek";
+  static const String lMinusOneTime = "-1 Sek";
+
+// action_menu.dart
+  static const String lGameStartErrorMessage =
+      "Fehler: Spiel wurde noch nicht gesartet!";
+  static const String lOffensePopUpHeader = "Offensive Aktionen";
+  static const String lDeffensePopUpHeader = "Defensive Aktionen";
+  static const String lSelectActionDialogHeader = "Wählen Sie eine Aktion";
+
+  // Pop ups
+  static const String lNoAssist = "Kein Assist";
+
+
+  // game_actions.dart & action_feed.dart
+  static const String lRedCard = "Rote Karte";
+  static const String lYellowCard = "Gelbe Karte";
+  static const String lTimePenalty = "Zeitstrafe";
+  static const String lGoal = "Tor";
+  static const String lOneVsOneAnd7m = "1v1 & 7m";
+  static const String lTwoMin = "2min ziehen";
+  static const String lErrThrow = "err"; // TODO correct name
+  static const String lTrf = "TRF";
+  static const String lFoul7m = "Foul => 7m";
+  static const String lBlockNoBall = "Block ohne Ballgewinn";
+  static const String lBlockAndSteal = "Block & Steal";
+
+}
---
+++ Code/flutter/handball_performance_tracker/lib/constants/stringsGeneral.dart
@@ -0,0 +1,103 @@
+class StringsGeneral {
+// all language strings start with a small l in front of the variable
+
+  // main.dart
+  static const String lAppTitle = "HB Performance";
+  static const String lError = "Error";
+  static const String lConnectionCheck = "Checking connection...";
+  static const String lConnected = "connected";
+  static const String lNot = "not";
+
+  // nav_drawer.dart
+  static const String lSideMenuHeader = "Side menu";
+  static const String lSettings = "Settings";
+  static const String lMainScreen = "Main Screen";
+  static const String lHelperScreen = "Helper Screen";
+  static const String lTeamSelectionScreen = "Team Auswahl";
+
+
+  // game_actions.dart & action_feed.dart
+  static const String lRedCard = "Rote Karte";
+  static const String lYellowCard = "Gelbe Karte";
+  static const String lTimePenalty = "Zeitstrafe";
+  static const String lGoal = "Tor";
+  static const String lOneVsOneAnd7m = "1v1 & 7m";
+  static const String lTwoMin = "2min ziehen";
+  static const String lErrThrow = "err"; // TODO correct name
+  static const String lTrf = "TRF";
+  static const String lFoul7m = "Foul => 7m";
+  static const String lBlockNoBall = "Block ohne Ballgewinn";
+  static const String lBlockAndSteal = "Block & Steal";
+
+
+
+  // mainScreen.dart
+  static const String lGameScreenHeader = "Game mode";
+  static const String lFeedHeader = "Feed";
+
+  // helperScreen.dart
+  static const String lHelperScreenHeader = "Clock Example";
+
+  // action_feed.dart
+  static const String lPlayerID = "Player ID";
+
+  
+  // ef_score_bar.dart
+  static const String lEfScore = "EFSCORE";
+
+  // playermenu.dart
+  static const String lPlayer = "Player";
+
+
+  
+  // globalController.dart
+  static const String lGoalScreenText = "Assist2"; // not used
+
+
+
+
+
+  // team_type_selection_bar.dart
+  static const String lMenTeams = "Herren";
+  static const String lWomenTeams = "Damen";
+  static const String lYouthTeams = "Jugend";
+
+  // team_details_form.dart
+
+  static const String lProcessingData = "Verarbeite Daten";
+
+  // team_settings_bar.dart
+  static const String lGames = "Spiele";
+  static const String lTeamDetails = "Team Details";
+
+  // start game screen
+  static const String lBack = "Zurück";
+  static const String lNext = "Weiter";
+  static const String lSeason = "Saison";
+  static const String lTeam = "Mannschaft";
+  static const String lDate = "Datum";
+  static const String lOpponent = "Gegner";
+  static const String lLocation = "Halle";
+  static const String lHomeGame = "Heimspiel";
+  static const String lOutwardsGame = "Auswärtsspiel";
+  static const String lStartGameFormPrompt =
+      "Bitte ergänze alle fehlenden Felder";
+  static const String lStartGameTitle = "Neues Spiel tracken";
+  static const String lStartGameTeamPrompt = "Bitte wähle ein Team aus";
+  static const String lStartGamePlayerSelectionPrompt =
+      "Bitte wähle alle Spieler für den Spieltag aus";
+  static const String lStartGamePlayerPositioningPrompt =
+      "Bitte wähle die Startaufstellung aus den nominierten Spielern aus";
+  static const String lHotReloadError =
+      "Es scheint ein Problem zu geben. Bitte gehen Sie zurück zum Dashboard";
+
+  
+  // team settings screen
+  static const String lName = "Name";
+  static const String lNumber = "Nummer";
+  static const String lPosition = "Position";
+
+  static const String lFirstName = "Vorname";
+  static const String lLastName = "Nachname";
+  
+}
