b57cd86fb4228fff3f71da1ca77c4928f703972a
Annika Mücke <annika-muecke@outlook.de>
2022-07-18 15:55:10 +0200

    move menu bar to start_game_form widget to enable embedding of season dropdown

--- Code/flutter/handball_performance_tracker/lib/screens/startGameScreen.dart
+++ Code/flutter/handball_performance_tracker/lib/screens/startGameScreen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:handball_performance_tracker/controllers/tempController.dart';
-import 'package:handball_performance_tracker/screens/dashboard.dart';
 import 'package:handball_performance_tracker/widgets/start_game_screen/start_game_form.dart';
-import 'package:handball_performance_tracker/constants/stringsDashboard.dart';
 import 'package:handball_performance_tracker/constants/stringsGeneral.dart';
 import '../widgets/nav_drawer.dart';
 
@@ -36,33 +34,9 @@ class _StartGameScreenState extends State<StartGameScreen> {
       ));
     return SafeArea(
         child: Scaffold(
-            key: _scaffoldKey,
-            drawer: NavDrawer(),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Container for menu button on top left corner
-                MenuButton(_scaffoldKey),
-                buildStartScreenContent(),
-                buildBackButton()
-              ],
-            )));
-  }
-
-  /// Depending on what page of the screen you are render different main content
-  Widget buildStartScreenContent() {
-    return StartGameForm();
-  }
-
-  // Button back to Dashboard, button leading to the next form is the submit-button of the StartGameForm
-  Widget buildBackButton() {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      // 'Back' button
-      ElevatedButton(
-          onPressed: () {
-            Get.to(() => Dashboard());
-          },
-          child: Text(StringsDashboard.lDashboard))
-    ]);
+      key: _scaffoldKey,
+      drawer: NavDrawer(),
+      body: StartGameForm(_scaffoldKey),
+    ));
   }
 }
