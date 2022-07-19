387268d063d6399b1a0933e3360ef3f3d68c1f47
Annika Mücke <annika-muecke@outlook.de>
2022-07-19 11:33:22 +0200

    add reset password functionality

--- Code/flutter/handball_performance_tracker/lib/widgets/authentication_screen/login_widget.dart
+++ Code/flutter/handball_performance_tracker/lib/widgets/authentication_screen/login_widget.dart
@@ -117,5 +117,134 @@ class _LoginWidgetState extends State<LoginWidget> {
             ),
           ],
         ));
+  Future signIn() async {
+    // show an indication that user is signing in
+    var progress = Alert(
+      context: context,
+      content: Container(
+        alignment: AlignmentDirectional.center,
+        decoration: BoxDecoration(
+          color: Colors.white70,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.blue[200],
+              borderRadius: BorderRadius.circular(10.0)),
+          width: 300.0,
+          height: 200.0,
+          alignment: AlignmentDirectional.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: SizedBox(
+                  height: 50.0,
+                  width: 50.0,
+                  child: CircularProgressIndicator(
+                    value: null,
+                    strokeWidth: 7.0,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 25.0),
+                child: Center(
+                  child: Text(
+                    "loading.. wait...",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    Alert loadingAlert = Alert(
+        context: context,
+        content: Column(
+          children: [
+            Text(StringsAuth.lLoggingIn),
+            CircularProgressIndicator(color: buttonDarkBlueColor),
+          ],
+        ),
+        buttons: []);
+
+    progress.show();
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim());
+      // pop the indicator after account has been created
+      loadingAlert.dismiss();
+    } on FirebaseAuthException catch (e) {
+      loadingAlert.dismiss();
+      Alert(context: context, content: Text(e.message.toString())).show();
+      // pop alert once it sign in is completed
+
+    }
+  }
+
+  Future signUp() async {
+    // show an indication that user is signing up
+    Alert loadingAlert = Alert(
+        context: context,
+        content: Column(
+          children: [
+            Text(StringsAuth.lSigningUp),
+            CircularProgressIndicator(),
+          ],
+        ),
+        buttons: []);
+    loadingAlert.show();
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim());
+      // pop the indicator after account has been created
+      loadingAlert.dismiss();
+    } on FirebaseAuthException catch (e) {
+      loadingAlert.dismiss();
+      Alert(context: context, content: Text(e.message.toString())).show();
+    }
+  }
+
+  void onClickedSignUp() {
+    // switch to sign up view
+    setState(() {
+      isLogin = !isLogin;
+    });
+  }
+
+  void onClickedReset() {
+    // switch to password reset view
+    setState(() {
+      isReset = !isReset;
+    });
+  }
+
+  void sendPasswordResetEmail() async {
+    // show an indication that user is signing up
+    Alert loadingAlert = Alert(
+        context: context,
+        content: Column(
+          children: [
+            Text(StringsAuth.lSendingResetMail),
+            CircularProgressIndicator(),
+          ],
+        ),
+        buttons: []);
+    loadingAlert.show();
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text.trim());
+      loadingAlert.dismiss();
+    } on FirebaseAuthException catch (e) {
+      loadingAlert.dismiss();
+      Alert(context: context, content: Text(e.message.toString())).show();
+    }
+    onClickedReset();
   }
 }
