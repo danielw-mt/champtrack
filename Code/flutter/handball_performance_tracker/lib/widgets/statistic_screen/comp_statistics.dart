875fdd56c509e7bbfdcf732ce75366224e35b8bc
Vincent <vincent.gemvin.gemar@fau.de>
2022-08-22 22:06:11 +0200

    ADDED basic hardcoded layout of comparison screen

--- Code/flutter/handball_performance_tracker/lib/widgets/statistic_screen/comp_statistics.dart
+++ Code/flutter/handball_performance_tracker/lib/widgets/statistic_screen/comp_statistics.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
-
+import 'statistic_card_elements.dart';
+import 'charts.dart';
 
 class ComparisonStatistics extends StatelessWidget {
-  const ComparisonStatistics({Key? key, required this.changePage}) : super(key: key);
+  const ComparisonStatistics({Key? key, required this.changePage})
+      : super(key: key);
   final void Function(int) changePage;
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text('Page', style: Theme.of(context).textTheme.headline6),
-          ElevatedButton(
-            onPressed: () => changePage(0),
-            child: const Text('Switch to Home Page'),
-          )
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        // Payer List in this Team
+        Expanded(flex: 1, child: PlayerList()),
+        // Quotes
+        Flexible(
+          flex: 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Card(child: LineChartWidget()),
+              ),
+              Flexible(
+                flex: 1,
+                child: Card(child: PerformanceCard()),
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: CardsInfoCard(),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Card(
+                    child: Center(child: Text("Game field coming soon")),
+                  ),
+                )
+              ],
+            )),
+      ],
     );
   }
 }
