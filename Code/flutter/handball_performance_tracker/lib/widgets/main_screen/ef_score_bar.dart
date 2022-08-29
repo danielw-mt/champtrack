089d64d6877cb535ac04b1c924cd0a40639a9f7a
Daniel Wagner <Dan.wagner@fau.de>
2022-08-29 13:22:07 +0200

    Implement greying out button

--- Code/flutter/handball_performance_tracker/lib/widgets/main_screen/ef_score_bar.dart
+++ Code/flutter/handball_performance_tracker/lib/widgets/main_screen/ef_score_bar.dart
@@ -102,16 +102,16 @@ class EfScoreBar extends StatelessWidget {
     return GetBuilder<TempController>(
         id: "ef-score-bar",
         builder: (tempController) {
-    List<Container> buttons = [];
-    for (int i = 0; i < getOnFieldIndex().length; i++) {
+          List<Container> buttons = [];
+          for (int i = 0; i < getOnFieldIndex().length; i++) {
             Container button = buildPlayerButton(context, i, tempController);
-      buttons.add(button);
-    }
-    return ButtonBar(
-      buttons: buttons,
-      width: scorebarWidth,
-      padWidth: 0.0,
-    );
+            buttons.add(button);
+          }
+          return ButtonBar(
+            buttons: buttons,
+            width: scorebarWidth,
+            padWidth: 0.0,
+          );
         });
   }
 }
@@ -181,7 +181,8 @@ void showPopup(BuildContext context, List<Container> buttons, int i) {
 /// @param i: Index of player that is represented in playerBarPlayers
 /// @return Container with TextButton representing the player.
 ///         On pressing the button a new popup with possible substitute player pops up.
-Container buildPlayerButton(BuildContext context, int i, TempController tempController) {
+Container buildPlayerButton(
+    BuildContext context, int i, TempController tempController) {
   // Get player which have at least one of the given positions.
   List<Player> playerWithSamePosition(List<String> positions) {
     List<Player> substitutePlayer = [];
@@ -246,17 +247,17 @@ Container buildPlayerButton(BuildContext context, int i, TempController tempCont
     height: buttonHeight,
     child: Stack(
       children: [
-              getButton(
-                  tempController.getPlayersFromSelectedTeam()[
-                      tempController.getPlayerBarPlayers()[i]],
-                  tempController),
+        getButton(
+            tempController.getPlayersFromSelectedTeam()[
+                tempController.getPlayerBarPlayers()[i]],
+            tempController),
         SizedBox(
           height: buttonHeight,
           width: scorebarButtonWidth,
           child: TextButton(
             child: const Text(""),
             onPressed: () {
-                    popupSubstitutePlayer(tempController);
+              popupSubstitutePlayer(tempController);
             },
             style: TextButton.styleFrom(
               // Color of pressed player changes on efscore bar.
@@ -312,8 +313,7 @@ Container buildPopupPlayerButton(
     child: Stack(
       children: [
         getButton(
-            tempController.getPlayersFromSelectedTeam()[i], 
-                  tempController),
+            tempController.getPlayersFromSelectedTeam()[i], tempController),
         SizedBox(
           height: buttonHeight,
           width: scorebarButtonWidth,
@@ -342,6 +342,26 @@ Container buildPopupPlayerButton(
 }
 
 Row getButton(Player player, TempController tempController) {
+  // TODO implement GetBuilder with id associated to time penalty
+
+  // start: deal with penalized players
+  final PersistentController persController = Get.find<PersistentController>();
+  bool penaltyIsActive = false;
+  if (tempController.isPlayerPenalized(player)) {
+    int penaltySince = tempController.playerIsPenalizedSince(player);
+    if (persController.getCurrentGame().stopWatch.rawTime.value - penaltySince <
+        60000) {
+      penaltyIsActive = true;
+    } else {
+      tempController.removePenalizedPlayer(player);
+    }
+  }
+  // end: deal with penalized players
+  if (penaltyIsActive) {
+    buttonColor = Colors.grey;
+  } else {
+    buttonColor = Colors.white;
+  }
   return Row(
     children: [
       // Playernumber
