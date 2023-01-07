5ad68a888164d7d710586bdcaf53d40ec2c4dc99
Vincent <vincent.gemvin.gemar@fau.de>
2023-01-07 16:00:41 +0100

    commented out Logger

--- Code/flutter/handball_performance_tracker/lib/features/statistics/widgets/statistic_dropdowns.dart
+++ Code/flutter/handball_performance_tracker/lib/features/statistics/widgets/statistic_dropdowns.dart
@@ -6,18 +6,18 @@ import 'package:handball_performance_tracker/features/statistics/bloc/statistics
 import 'package:handball_performance_tracker/data/models/player_model.dart';
 import 'package:handball_performance_tracker/data/models/game_model.dart';
 import 'package:handball_performance_tracker/data/models/team_model.dart';
-import 'package:logger/logger.dart';
+// import 'package:logger/logger.dart';
 
-var logger = Logger(
-  printer: PrettyPrinter(
-      methodCount: 2, // number of method calls to be displayed
-      errorMethodCount: 8, // number of method calls if stacktrace is provided
-      lineLength: 120, // width of the output
-      colors: true, // Colorful log messages
-      printEmojis: true, // Print an emoji for each log message
-      printTime: true // Should each log print contain a timestamp
-      ),
-);
+// var logger = Logger(
+//   printer: PrettyPrinter(
+//       methodCount: 2, // number of method calls to be displayed
+//       errorMethodCount: 8, // number of method calls if stacktrace is provided
+//       lineLength: 120, // width of the output
+//       colors: true, // Colorful log messages
+//       printEmojis: true, // Print an emoji for each log message
+//       printTime: true // Should each log print contain a timestamp
+//       ),
+// );
 
 
 class PlayerSelector extends StatelessWidget {
