fb55eb8b747e1c42b986c993a3358443a2175349
Daniel Wagner <Dan.wagner@fau.de>
2023-11-01 16:02:35 +0100

    make some changes to state management

--- Code/flutter/handball_performance_tracker/lib/features/statistics/view/statistics_view.dart
+++ Code/flutter/handball_performance_tracker/lib/features/statistics/view/statistics_view.dart
@@ -11,11 +11,15 @@ const String page3 = "Vergleich";
 class StatisticsView extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  // init statistic bloc on first build
+
   @override
   Widget build(BuildContext context) {
     final statisticsBloc = context.watch<StatisticsBloc>();
+
     print("statistics view: ${statisticsBloc.state.status}");
-    if (statisticsBloc.state.status == StatisticsStatus.loading) {
+    if (statisticsBloc.state.status != StatisticsStatus.loaded) {
+      print("loading");
       return const Center(child: CircularProgressIndicator());
     }
     return SafeArea(
